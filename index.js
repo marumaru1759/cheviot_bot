e9da9626abafbadc6a1b36043c22ac1720770522
marumaru1759 <marum1759@gmail.com>
2019-05-23 23:05:57 +0900

    backslash

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function handleMessage(sender_psid, received_message){
 	if(received_message.text){
 
 		response = {
-			"text": 'You sent the message: "${received_message.text}" Now send me an image!'
+			"text": `You sent the message: "${received_message.text}" Now send me an image!`
 		}
 	}
 
